f46cd6e52a00ad57189d4c91b4328d9f7255726b
Dmytriy Chuvardynskyi <dmytriy.chuvardynskyi@djangostars.com>
2019-08-02 18:49:13 +0300

    create Logo component

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Logo from '../../Logo/Logo'
+
 import classes from './Toolbar.css'
 
 
@@ -7,7 +9,7 @@ const toolbar = (props) => {
     return (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo/>
         <nav>
             ...
         </nav>
